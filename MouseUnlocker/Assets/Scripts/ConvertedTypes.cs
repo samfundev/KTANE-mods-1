02814c1adabb1b3bae121cf6bbcaf3f8c5756adb
caitsith2 <d_good@caitsith2.com>
2017-09-24 22:18:34 -0700

    MouseUnlocker Initial commit

---
+++ MouseUnlocker/Assets/Scripts/ConvertedTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+public static class CommonReflectedTypeInfo
+{
+    /*static CommonReflectedTypeInfo()
+    {
+        MouseControlsType = ReflectionHelper.FindType("MouseControls");
+        MaxBuzzerTimeField = AlarmClockType.GetField("MaxAlarmClockBuzzerTime", BindingFlags.Public | BindingFlags.Instance);
+    }
+
+    public static Type MouseControlsType
+    {
+        get;
+        private set;
+    }
+
+    public static FieldInfo hasFocusField
+    {
+        get;
+        private set;
+    }
+
+    public static MethodInfo FreeCursorMethod
+    {
+        get;
+        private set;
+    }*/
+}
---
+++ MouseUnlocker/Assets/Scripts/MouseUnlocker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Object = UnityEngine.Object;
+
+public class MouseUnlocker : MonoBehaviour
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool ClipCursor(IntPtr rect);
+
+    public KMService Service = null;
+
+    void Start()
+    {
+        Debug.Log("[MouseUnlocker] Started");
+        Update();
+    }
+
+    void Awake()
+    {
+        Debug.Log("[MouseUnlocker] is Awake.");
+        Update();
+    }
+
+    private bool _firstRun;
+    void Update()
+    {
+        if (!_firstRun)
+            Debug.Log("[MouseUnlocker] Unlocking the cursor");
+        ClipCursor(IntPtr.Zero);
+        if (!_firstRun)
+        {
+            Debug.Log("[MouseUnlocker] Cursor unlocked");
+            _firstRun = true;
+        }
+    }
+}
