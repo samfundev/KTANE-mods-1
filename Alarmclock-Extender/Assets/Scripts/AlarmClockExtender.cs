69a995a12116d54b61795b9ecd1e223958f4d652
caitsith2 <d_good@caitsith2.com>
2017-10-10 10:26:58 -0700

    Alarm clock now shuts off automatically on shorter clips

--- Alarmclock-Extender/Assets/Scripts/AlarmClockExtender.cs
+++ Alarmclock-Extender/Assets/Scripts/AlarmClockExtender.cs
@@ -17,13 +17,13 @@ public class AlarmClockExtender : MonoBehaviour
     }
 
     // Use this for initialization
-    void Start ()
-	{
+    void Start()
+    {
         DebugLog("Starting service");
-	    _gameInfo = GetComponent<KMGameInfo>();
-	    _gameInfo.OnStateChange += OnStateChange;
-	    _modSettings.ReadSettings();
-	}
+        _gameInfo = GetComponent<KMGameInfo>();
+        _gameInfo.OnStateChange += OnStateChange;
+        _modSettings.ReadSettings();
+    }
 
     void OnStateChange(KMGameInfo.State state)
     {
@@ -40,11 +40,18 @@ public class AlarmClockExtender : MonoBehaviour
         }
     }
 
+    private IEnumerator ShutOffAlarmClock(float time, Object AlarmClock)
+    {
+        yield return new WaitForSeconds(time);
+        CommonReflectedTypeInfo.TurnOffMethod.Invoke(AlarmClock, null);
+    }
+
     private IEnumerator CheckForAlarmClock()
     {
         yield return null;
         DebugLog("Attempting to Find Alarm Clock");
         _modSettings.ReadSettings();
+        Object AlarmClock = null;
 
         while (true)
         {
@@ -55,12 +62,67 @@ public class AlarmClockExtender : MonoBehaviour
                 continue;
             }
 
-            foreach(var alarm in alarmClock) { 
+            foreach (var alarm in alarmClock)
+            {
                 DebugLog("Alarm Clock found - Hooking into it.");
                 CommonReflectedTypeInfo.MaxBuzzerTimeField.SetValue(alarm, _modSettings.Settings.AlarmClockBuzzerTime);
                 DebugLog("Done setting up the Alarm Clocks");
-                yield break;
+                AlarmClock = alarm;
+                break;
             }
+            break;
+        }
+        if (AlarmClock == null)
+            yield break;
+        while (true)
+        {
+            if ((bool) CommonReflectedTypeInfo.BuzzerStateField.GetValue(AlarmClock))
+            {
+                float clipTime = -1;
+                AudioSource varAudio = null;
+                yield return new WaitForSeconds(0.1f);
+                try
+                {
+                    DebugLog("Alarm Clock just turned on. Trying to find out how long the clip length is.");
+                    var playingSound = CommonReflectedTypeInfo.PlayResultField.GetValue(AlarmClock);
+                    DebugLog("Retrieved the playingSound object: type = {0}", playingSound == null ? "null" : playingSound.GetType().FullName );
+                    PropertyInfo soundGroupVariationField = playingSound.GetType().GetProperty("ActingVariation");
+                    var soundGroupVariation = soundGroupVariationField.GetValue(playingSound, null);
+                    DebugLog("Retrieved the Acting Variation object: type = {0}", soundGroupVariation == null ? "null" : soundGroupVariation.GetType().FullName);
+                    PropertyInfo varAudioField = soundGroupVariation.GetType().GetProperty("VarAudio");
+                    varAudio = (AudioSource)varAudioField.GetValue(soundGroupVariation, null);
+                    DebugLog("Retrieved AudioSource: type = {0}", varAudio == null ? "null" : varAudio.name);
+                    if (varAudio != null && varAudio.clip != null)
+                    {
+                        clipTime = varAudio.clip.length;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DebugLog("Failed to get Clip length due to Exception: {0}\n{1}", ex.Message, ex.StackTrace);
+                    clipTime = -1;
+                }
+                if (clipTime > 0)
+                {
+                    Coroutine shutoff = StartCoroutine(ShutOffAlarmClock(clipTime, AlarmClock));
+                    DebugLog("Alarm Clock will play for {0} seconds provided it isn't shut off sooner. Kappa", Mathf.CeilToInt(Mathf.Min(clipTime, _modSettings.Settings.AlarmClockBuzzerTime)));
+                    while ((bool) CommonReflectedTypeInfo.BuzzerStateField.GetValue(AlarmClock))
+                    {
+                        yield return null;
+                    }
+                    if(shutoff != null)
+                        StopCoroutine(shutoff);
+                }
+                else
+                {
+                    DebugLog("Alarm Clock will play for {0} seconds provided it isn't shut off sooner. Kappa", Mathf.CeilToInt(_modSettings.Settings.AlarmClockBuzzerTime));
+                    while ((bool) CommonReflectedTypeInfo.BuzzerStateField.GetValue(AlarmClock))
+                    {
+                        yield return null;
+                    }
+                }
+            }
+            yield return null;
         }
     }
 }
--- Alarmclock-Extender/Assets/Scripts/ConvertedTypes.cs
+++ Alarmclock-Extender/Assets/Scripts/ConvertedTypes.cs
@@ -7,6 +7,9 @@ public static class CommonReflectedTypeInfo
     {
         AlarmClockType = ReflectionHelper.FindType("Assets.Scripts.Props.AlarmClock");
         MaxBuzzerTimeField = AlarmClockType.GetField("MaxAlarmClockBuzzerTime", BindingFlags.Public | BindingFlags.Instance);
+        BuzzerStateField = AlarmClockType.GetField("isOn", BindingFlags.NonPublic | BindingFlags.Instance);
+        PlayResultField = AlarmClockType.GetField("alarmClockBuzzerSound", BindingFlags.NonPublic | BindingFlags.Instance);
+        TurnOffMethod = AlarmClockType.GetMethod("TurnOff", BindingFlags.Public | BindingFlags.Instance);
     }
 
     #region AlarmClock
@@ -21,5 +24,24 @@ public static class CommonReflectedTypeInfo
         get;
         private set;
     }
+
+    public static FieldInfo BuzzerStateField
+    {
+        get;
+        private set;
+    }
+
+    public static FieldInfo PlayResultField
+    {
+        get;
+        private set;
+    }
+
+    public static MethodInfo TurnOffMethod
+    {
+        get;
+        private set;
+    }
+
     #endregion
 }
